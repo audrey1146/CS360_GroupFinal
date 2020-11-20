0db3c6eb224c215323c9da7311e7eb2129b1e9b9
Audrey Lincoln <audrey1146@gmail.com>
2020-11-20 09:43:46 -0800

    Working on landing and login UI

--- groupFinal/routes/index.js
+++ groupFinal/routes/index.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET landing page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) 
+{
+  // Randomly select one of the images
+
   res.render('landing');
+});
+
+/* GET landing page. */
+router.get('/login', function(req, res, next) 
+{
+  // Randomly select one of the images
+
+  res.render('login');
 });
 
 module.exports = router;
