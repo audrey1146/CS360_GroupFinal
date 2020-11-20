9252a5e71504fc9d6b81218b72daecb8a8cbb750
Audrey Lincoln <audrey1146@gmail.com>
2020-11-19 16:22:28 -0800

    Pug development for the landing and login

--- groupFinal/routes/index.js
+++ groupFinal/routes/index.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-// Require our controllers.
-var topping_controller = require('../controllers/toppingController'); 
-
-/* GET home page. */
+/* GET landing page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Slice of Pacific' });
+  res.render('landing');
 });
 
 module.exports = router;
