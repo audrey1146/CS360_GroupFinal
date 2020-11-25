1d67455bd4ae0c180996044a087790cc7478e1e4
Audrey Lincoln <audrey1146@gmail.com>
2020-11-24 20:56:12 -0800

    scaling and landing done

--- groupFinal/routes/index.js
+++ groupFinal/routes/index.js
@@ -4,17 +4,27 @@ var router = express.Router();
 /* GET landing page. */
 router.get('/', function(req, res, next) 
 {
-  // Randomly select one of the images
-
   res.render('landing');
 });
 
 /* GET landing page. */
 router.get('/login', function(req, res, next) 
 {
-  // Randomly select one of the images
-
   res.render('login');
 });
+
+/* GET landing page. */
+router.get('/register', function(req, res, next) 
+{
+  res.render('register');
+});
+
+/* GET request for user. */
+router.get('/home', function(req, res, next) 
+{
+  res.render('register');
+});
+
+
 
 module.exports = router;
